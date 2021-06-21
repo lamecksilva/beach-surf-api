8705c9ce2bdc1301de70d00552204f3ef2a51c40
Lameck Sandro <lameck2@hotmail.com>
2021-06-21 20:07:56 -0300

    FEAT: Add Prettier and fix files

--- src/server.ts
+++ src/server.ts
@@ -4,26 +4,26 @@ import json, { Application } from 'express';
 import { ForecastController } from './controllers/forecast';
 
 export class SetupServer extends Server {
-	constructor(private port = 3000) {
-		super();
-	}
+  constructor(private port = 3000) {
+    super();
+  }
 
-	public init(): void {
-		this.setupExpress();
-		this.setupControllers();
-	}
+  public init(): void {
+    this.setupExpress();
+    this.setupControllers();
+  }
 
-	private setupExpress(): void {
-		this.app.use(json());
-	}
+  private setupExpress(): void {
+    this.app.use(json());
+  }
 
-	private setupControllers(): void {
-		const forecastController = new ForecastController();
+  private setupControllers(): void {
+    const forecastController = new ForecastController();
 
-		this.addControllers([forecastController]);
-	}
+    this.addControllers([forecastController]);
+  }
 
-	public getApp(): Application {
-		return this.app;
-	}
+  public getApp(): Application {
+    return this.app;
+  }
 }
