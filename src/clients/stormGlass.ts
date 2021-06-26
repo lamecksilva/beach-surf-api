907e4b2a969baf4bf279a1eeef513ba81d542eac
Lameck Sandro <lameck2@hotmail.com>
2021-06-25 22:19:51 -0300

    FEAT: Jest with Typescript mock axios

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,7 +1,7 @@
 import { AxiosStatic } from 'axios';
 
 export interface StormGlassPointSource {
-  [key: string] : number
+  [key: string]: number;
 }
 export interface StormGlassPoint {
   readonly time: string;
@@ -9,24 +9,24 @@ export interface StormGlassPoint {
   readonly swellHeight: StormGlassPointSource;
   readonly swellPeriod: StormGlassPointSource;
   readonly waveHeight: StormGlassPointSource;
-  readonly waveDirection:StormGlassPointSource
-  readonly windDirection:StormGlassPointSource
-  readonly windSpeed:StormGlassPointSource
+  readonly waveDirection: StormGlassPointSource;
+  readonly windDirection: StormGlassPointSource;
+  readonly windSpeed: StormGlassPointSource;
 }
 
 export interface StormGlassForecastResponse {
-  hours: StormGlassPoint[]
+  hours: StormGlassPoint[];
 }
 
-export interface ForecastPoint{
+export interface ForecastPoint {
   time: string;
-   swellDirection: number;
-   swellHeight: number;
-   swellPeriod: number;
-   waveHeight: number;
-   waveDirection:number
-   windDirection:number
-   windSpeed:number
+  swellDirection: number;
+  swellHeight: number;
+  swellPeriod: number;
+  waveHeight: number;
+  waveDirection: number;
+  windDirection: number;
+  windSpeed: number;
 }
 
 export class StormGlass {
@@ -37,15 +37,22 @@ export class StormGlass {
   constructor(protected request: AxiosStatic) {}
 
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
-    const response=  await this.request.get<StormGlassForecastResponse>(
-      `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&lat=${lat}&lng=${lng}`
+    const response = await this.request.get<StormGlassForecastResponse>(
+      `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&lat=${lat}&lng=${lng}`,
+      {
+        headers: {
+          Authorization: 'fake-token',
+        },
+      }
     );
 
-    return this.normalizeReponse(response.data)
+    return this.normalizeReponse(response.data);
   }
 
   // Normalize (format) the StormGlass API JSON to our JSON shape
-  private normalizeReponse(points: StormGlassForecastResponse): ForecastPoint[] {
+  private normalizeReponse(
+    points: StormGlassForecastResponse
+  ): ForecastPoint[] {
     // Idk yet the use of bind
     return points.hours.filter(this.isValidPoint.bind(this)).map((point) => ({
       swellDirection: point.swellDirection[this.stormGlassAPISource],
@@ -60,8 +67,8 @@ export class StormGlass {
   }
 
   // Partial: Make all properties optional
-  private isValidPoint(point: Partial<StormGlassPoint>):boolean {
-  // Check if the point has all Keys
+  private isValidPoint(point: Partial<StormGlassPoint>): boolean {
+    // Check if the point has all Keys
     return !!(
       point.time &&
       point.swellDirection?.[this.stormGlassAPISource] &&
@@ -73,5 +80,4 @@ export class StormGlass {
       point.windSpeed?.[this.stormGlassAPISource]
     );
   }
-
 }
