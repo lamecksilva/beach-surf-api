735334ca8bf5c8cc8688d520a0253c9293a84eae
Lameck Sandro <lameck2@hotmail.com>
2021-07-08 21:15:16 -0300

    FEAT: Add mongoose and mongoDB connection

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import './util/module-alias';
 import { Server } from '@overnightjs/core';
 import json, { Application } from 'express';
 import { ForecastController } from './controllers/forecast';
+import * as database from './database';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -11,6 +12,7 @@ export class SetupServer extends Server {
   public init(): void {
     this.setupExpress();
     this.setupControllers();
+    this.databaseSetup();
   }
 
   private setupExpress(): void {
@@ -21,6 +23,14 @@ export class SetupServer extends Server {
     const forecastController = new ForecastController();
 
     this.addControllers([forecastController]);
+  }
+
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
   }
 
   public getApp(): Application {
--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,8 +1,12 @@
 import { SetupServer } from '@src/server';
 import supertest from 'supertest';
 
-beforeAll(() => {
-  const server = new SetupServer();
+let server: SetupServer;
+
+beforeAll(async () => {
+  server = new SetupServer();
   server.init();
   global.testRequest = supertest(server.getApp());
 });
+
+afterAll(async () => await server.close());
