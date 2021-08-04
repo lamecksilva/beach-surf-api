ede9794e24e44ce8c12d343334a6d2988b572ce3
Lameck Sandro <lameck2@hotmail.com.br>
2021-08-04 15:32:52 -0300

    FEAT: Create User's functional test, model, controller

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import * as database from './database';
 import { ForecastController } from './controllers/forecast';
 import { BeachesController } from './controllers/beaches';
 import { json } from 'body-parser';
+import { UsersController } from './controllers/user';
 
 export class SetupServer extends Server {
   constructor(private port = 9000) {
@@ -24,8 +25,9 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const forecastController = new ForecastController();
     const beachesController = new BeachesController();
+    const usersController = new UsersController()
 
-    this.addControllers([forecastController, beachesController]);
+    this.addControllers([forecastController, beachesController, usersController]);
   }
 
   private async databaseSetup(): Promise<void> {
