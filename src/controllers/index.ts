01ff6a955b67ad4498c7e43d87c5a369da583fcd
Lameck Sandro <lameck2@hotmail.com.br>
2021-08-05 00:05:41 -0300

    FEAT: Type narrowing

---
+++ src/controllers/index.ts
@@ -0,0 +1,42 @@
+import { CUSTOM_VALIDATION } from '@src/models/user';
+import { Response } from 'express';
+import Mongoose from 'mongoose';
+
+// Abstract classes can only be extended
+export abstract class BaseController {
+  protected sendCreateUpdateErrorResponse(
+    res: Response,
+    error: Mongoose.Error.ValidationError | Error
+  ): void {
+    if (error instanceof Mongoose.Error.ValidationError) {
+      const clientErrors = this.handleClientErrors(error);
+
+      res
+        .status(clientErrors.code)
+        .send({ code: clientErrors.code, error: clientErrors.error });
+    } else {
+      res.status(500).send({ code: 500, error: 'Something went wrong!' });
+    }
+  }
+
+  private handleClientErrors(error: Mongoose.Error.ValidationError): {
+    code: number;
+    error: string;
+  } {
+    const duplicatedKindErrors = Object.values(error.errors).filter((err) => {
+      if (
+        err instanceof Mongoose.Error.ValidatorError ||
+        err instanceof Mongoose.Error.CastError
+      ) {
+        return err.kind === CUSTOM_VALIDATION.DUPLICATED;
+      } else {
+        return null;
+      }
+    });
+
+    if (duplicatedKindErrors.length) {
+      return { code: 409, error: error.message };
+    }
+    return { code: 422, error: error.message };
+  }
+}
