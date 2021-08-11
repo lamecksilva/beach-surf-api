a5a1685d366b7abce1e4dde489587020cdfe10e6
Lameck Sandro <lameck2@hotmail.com>
2021-08-10 22:50:18 -0300

    FIX: Remove forecast

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,5 +1,4 @@
 import { ClassMiddleware, Controller, Get } from '@overnightjs/core';
-import logger from '@src/logger';
 import { authMiddleware } from '@src/middlewares/auth';
 import { Beach } from '@src/models/beach';
 import { Forecast } from '@src/services/forecast';
