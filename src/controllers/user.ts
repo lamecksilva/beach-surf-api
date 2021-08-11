a8d5206a504ad10d7390fa37408d567659022743
Lameck Sandro <lameck2@hotmail.com.br>
2021-08-11 15:25:04 -0300

    FEAT: Route me controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,9 +1,10 @@
-import { Controller, Post } from '@overnightjs/core';
+import { Controller, Get, Middleware, Post } from '@overnightjs/core';
 import { Response, Request } from 'express';
 import { User } from '@src/models/user';
 import { BaseController } from '.';
 import AuthService from '@src/services/auth';
 import logger from '@src/logger';
+import { authMiddleware } from '@src/middlewares/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -43,5 +44,21 @@ export class UsersController extends BaseController {
     const token = AuthService.generateToken(user.toJSON());
 
     return res.status(200).send({ token });
+  }
+
+  @Get('me')
+  @Middleware(authMiddleware)
+  public async me(req: Request, res: Response): Promise<Response> {
+    const email = req.decoded ? req.decoded.email : undefined;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return this.sendErrorResponse(res, {
+        code: 404,
+        message: 'User not found!',
+      });
+    }
+
+    return res.send({ user });
   }
 }
--- src/middlewares/__test__/auth.test.ts
+++ src/middlewares/__test__/auth.test.ts
@@ -2,7 +2,7 @@ import AuthService from '@src/services/auth';
 import { authMiddleware } from '../auth';
 
 describe('AuthMiddleware', () => {
-  it('should verify a JWT token and call the nex middleware', async () => {
+  it('should verify a JWT token and call the next middleware', async () => {
     const jwtToken = AuthService.generateToken({ data: 'fake' });
 
     const reqFake = {
