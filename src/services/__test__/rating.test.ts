931493f4e2587f9428864fa19704ae4226d53d69
Lameck Sandro <lameck2@hotmail.com.br>
2021-08-13 10:42:45 -0300

    FEAT: Rating service test pt.2

--- src/services/__test__/rating.test.ts
+++ src/services/__test__/rating.test.ts
@@ -47,22 +47,63 @@ describe('Rating Service', () => {
 
   describe('Get rating based on swell period', () => {
     it('should get a rating of 1 for a period of 5 seconds', () => {
-      const rating = defaultRating.getRatingForSwellPeriod(5)
-      expect(rating).toBe(1)
-    })
+      const rating = defaultRating.getRatingForSwellPeriod(5);
+      expect(rating).toBe(1);
+    });
     it('should get a rating of 2 for a period of 9 seconds', () => {
-      const rating = defaultRating.getRatingForSwellPeriod(9)
-      expect(rating).toBe(2)
-    })
+      const rating = defaultRating.getRatingForSwellPeriod(9);
+      expect(rating).toBe(2);
+    });
     it('should get a rating of 4 for a period of 12 seconds', () => {
-      const rating = defaultRating.getRatingForSwellPeriod(12)
-      expect(rating).toBe(4)
-    })
+      const rating = defaultRating.getRatingForSwellPeriod(12);
+      expect(rating).toBe(4);
+    });
     it('should get a rating of 5 for a period of 16 seconds', () => {
-      const rating = defaultRating.getRatingForSwellPeriod(16)
-      expect(rating).toBe(5)
+      const rating = defaultRating.getRatingForSwellPeriod(16);
+      expect(rating).toBe(5);
+    });
+  });
+
+  describe('Get rating based on the swell height', () => {
+    it('should get rating 1 for less then ankle to knee high swell', () => {
+      const rating = defaultRating.getRatingForSwellSize(0.2);
+      expect(rating).toBe(1);
+    });
+    it('should get rating 2 for an ankle to knee swell', () => {
+      const rating = defaultRating.getRatingForSwellSize(0.6);
+      expect(rating).toBe(2);
+    });
+    it('should get rating 3 for waist high swell', () => {
+      const rating = defaultRating.getRatingForSwellSize(1.5);
+      expect(rating).toBe(3);
+    });
+    it('should get rating 5 for overhead swell', () => {
+      const rating = defaultRating.getRatingForSwellSize(2.5);
+      expect(rating).toBe(5);
+    });
+  });
+
+  describe('Get position based on points location', () => {
+    it('should get the point based on a east location', () => {
+      const rating = defaultRating.getPositionFromLocation(92);
+      expect(rating).toBe(BeachPosition.E)
+    })
+    it('should get the point based on a north location 1', () => {
+      const rating = defaultRating.getPositionFromLocation(360);
+      expect(rating).toBe(BeachPosition.N)
+    })
+    it('should get the point based on a north location 2', () => {
+      const rating = defaultRating.getPositionFromLocation(40);
+      expect(rating).toBe(BeachPosition.N)
+    })
+    it('should get the point based on a south location', () => {
+      const rating = defaultRating.getPositionFromLocation(200);
+      expect(rating).toBe(BeachPosition.S)
+    })
+    it('should get the point based on a west location', () => {
+      const rating = defaultRating.getPositionFromLocation(300);
+      expect(rating).toBe(BeachPosition.W)
     })
 
   })
-  
 });
--- src/services/rating.ts
+++ src/services/rating.ts
@@ -1,5 +1,20 @@
 import { Beach, BeachPosition } from '@src/models/beach';
 
+const waveHeights = {
+  ankleToKnee: {
+    min: 0.3,
+    max: 1.0,
+  },
+  waistHigh: {
+    min: 1.0,
+    max: 2.0,
+  },
+  headHigh: {
+    min: 2.0,
+    max: 2.5,
+  },
+};
+
 export class Rating {
   constructor(private beach: Beach) {}
 
@@ -14,6 +29,42 @@ export class Rating {
     }
 
     return 3;
+  }
+
+  public getRatingForSwellSize(height: number): number {
+    if (
+      height >= waveHeights.ankleToKnee.min &&
+      height < waveHeights.ankleToKnee.max
+    ) {
+      return 2;
+    }
+    if (
+      height >= waveHeights.waistHigh.min &&
+      height < waveHeights.waistHigh.max
+    ) {
+      return 3;
+    }
+    if (height >= waveHeights.headHigh.min) {
+      return 5;
+    }
+
+    return 1;
+  }
+
+  public getPositionFromLocation(coordinates: number): BeachPosition {
+    if (coordinates >= 310 || (coordinates < 50 && coordinates >= 0)) {
+      return BeachPosition.N;
+    }
+    if (coordinates >= 50 && coordinates < 120) {
+      return BeachPosition.E;
+    }
+    if (coordinates >= 120 && coordinates < 220) {
+      return BeachPosition.S;
+    }
+    if (coordinates >= 220 && coordinates < 310) {
+      return BeachPosition.W;
+    }
+    return BeachPosition.E;
   }
 
   public getRatingForSwellPeriod(period: number): number {
